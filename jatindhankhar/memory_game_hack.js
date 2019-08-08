d6cf3f59fb5c34280d85c65a970ddba95e77e146
Jatin Dhankhar <2412081+jatindhankhar@users.noreply.github.com>
2019-08-08 20:44:12 +0530

    Make it more cool :sunglasses: ?
    We can use cleaner and concise ES6

--- jatindhankhar/memory_game_hack.js
+++ jatindhankhar/memory_game_hack.js
@@ -1,6 +1,7 @@
 classes = ['fa-cube', 'fa-diamond', 'fa-leaf', 'fa-bolt', 'fa-bomb', 'fa-anchor', 'fa fa-paper-plane-o', 'fa fa-bicycle']
 
-for(var i = 0 ; i < classes.length ; i++){
-	var x = document.getElementsByClassName(classes[i]);
-	for(var j=0; j <x.length ; j++){x[j].parentElement.click();}
+for(let targetClass of classes){
+	
+	targetElements = [...document.getElementsByClassName(targetClass)]
+	targetElements.map(el => el.parentElement.click());
 }
