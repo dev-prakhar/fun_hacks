a77e41b5544d053b0cee4b78a536b1ecca9fe092
Jatin Dhankhar <2412081+jatindhankhar@users.noreply.github.com>
2019-08-08 22:03:55 +0530

    Update memory_game_hack.js

--- jatindhankhar/memory_game_hack.js
+++ jatindhankhar/memory_game_hack.js
@@ -1,7 +1,6 @@
 classes = ['fa-cube', 'fa-diamond', 'fa-leaf', 'fa-bolt', 'fa-bomb', 'fa-anchor', 'fa fa-paper-plane-o', 'fa fa-bicycle']
 
-for(let targetClass of classes){
-	
-	targetElements = [...document.getElementsByClassName(targetClass)]
-	targetElements.map(el => el.parentElement.click());
+for (let targetClass of classes) {
+ targetElements = [...document.getElementsByClassName(targetClass)]
+ targetElements.map(el => el.parentElement.click());
 }
